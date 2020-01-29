9ed4a496802ab6cf0c1e733ba56b8da25ad581f1
Hersh Nagpal <hnagpal@wayfair.com>
2020-01-29 11:45:55 -0500

    Fixed bugs showing images of actors and started work on Favorites list

---
+++ WayMovies/Controller/FavoritesViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FavoritesViewController.swift
+//  WayMovies
+//
+//  Created by Hersh Nagpal on 1/29/20.
+//  Copyright © 2020 Hersh Nagpal. All rights reserved.
+//
+
+import Foundation
---
+++ WayMovies/View/FavoritesCollection.swift
@@ -0,0 +1,9 @@
+//
+//  FavoritesCollection.swift
+//  WayMovies
+//
+//  Created by Hersh Nagpal on 1/29/20.
+//  Copyright © 2020 Hersh Nagpal. All rights reserved.
+//
+
+import Foundation
